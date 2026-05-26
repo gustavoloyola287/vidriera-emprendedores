4bfc2b19bf6796367fb8075e53d757b2245242d8
gustavoloyola287 <168578001+gustavoloyola287@users.noreply.github.com>
2026-05-26 10:18:09 -0300

    Primer commit del proyecto

---
+++ src/main/java/ar/com/vidrieraemprendedores/vidrieraemprendedores/VidrieraemprendedoresApplication.java
@@ -0,0 +1,13 @@
+package ar.com.vidrieraemprendedores.vidrieraemprendedores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidrieraemprendedoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidrieraemprendedoresApplication.class, args);
+	}
+
+}
