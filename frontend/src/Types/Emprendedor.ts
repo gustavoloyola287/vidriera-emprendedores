129e7e0805c1326eacf4b6530467ae3f11961321
MarcosEzeArg <marcosezequielarganaraz@gmail.com>
2026-06-15 23:30:35 -0300

    Frontend
    Agrego Frontend

---
+++ frontend/src/Types/Emprendedor.ts
@@ -0,0 +1,9 @@
+export interface Emprendedor {
+    id: number;
+    nombre: string;
+    descripcion: string;
+    imagenUrl: string;
+    contacto: string;
+    redesSociales: string;
+    activo: boolean;
+}
