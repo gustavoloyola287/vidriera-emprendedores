8c86af98399e4abeb517cbff7590162edaa205f1
MarcosEzeArg <marcosezequielarganaraz@gmail.com>
2026-07-14 21:43:38 -0300

    Update CardEmprendedor.tsx

--- frontend/src/Components/CardEmprendedor.tsx
+++ frontend/src/Components/CardEmprendedor.tsx
@@ -1,9 +1,5 @@
 import { Emprendedor } from "../Types/Emprendedor";
 
 interface cardemprendedorprops{
-    imageurl : string;
     emprendedor:Emprendedor;
-    nombre : string;
-    descripcion : string;
-    
 }
