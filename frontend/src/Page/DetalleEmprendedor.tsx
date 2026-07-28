2c871e478c146c7ba97bf7dcdde80adc4515ffd7
MarcosEzeArg <marcosezequielarganaraz@gmail.com>
2026-07-28 19:27:03 -0300

    Update DetalleEmprendedor.tsx

--- frontend/src/Page/DetalleEmprendedor.tsx
+++ frontend/src/Page/DetalleEmprendedor.tsx
@@ -1,4 +1,15 @@
 import { Emprendedor } from "../Types/Emprendedor";
-interface DetalleEmprendedorprops Emprendedor {
-    
+interface DetalleEmprendedorprops  {
+    emprendedor : Emprendedor;
+}
+function DetalleEmprendedorprops (props: DetalleEmprendedorprops){
+
+    return(
+        <div>
+            <div><img src="" alt="" /></div>
+            <div><h1>props.emprendedor.nombre</h1></div>
+            <p>props.emprendedor.descripcion</p>
+        </div>
+    );
+
 }
