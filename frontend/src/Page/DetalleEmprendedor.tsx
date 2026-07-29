bd9ceaba47842db431c5f8cfe4d89aad9a91912b
MarcosEzeArg <marcosezequielarganaraz@gmail.com>
2026-07-29 12:01:01 -0300

    Update DetalleEmprendedor.tsx

--- frontend/src/Page/DetalleEmprendedor.tsx
+++ frontend/src/Page/DetalleEmprendedor.tsx
@@ -7,10 +7,13 @@ function DetalleEmprendedorprops (props: DetalleEmprendedorprops){
     return(
         <div>
             <div><img src={props.emprendedor.imagenUrl} alt={props.emprendedor.nombre} /></div>
-            <div><h1>{props.emprendedor.nombre}</h1></div>
+            <div><h1>{props.emprendedor.nombre}</h1>
             <p>{props.emprendedor.descripcion}</p>
-            <p>{props.emprendedor.contacto}</p>
+            </div>
+            <div>
+            <p><h2>{props.emprendedor.contacto}</h2></p>
             <p>{props.emprendedor.redesSociales}</p>
+            </div>
         </div>
     );
 
