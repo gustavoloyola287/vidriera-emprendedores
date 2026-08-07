b3f96745e2701936734e2d646711a5f84b4eb149
MarcosEzeArg <marcosezequielarganaraz@gmail.com>
2026-08-06 22:23:18 -0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
+import {browserRouter} from 'react-router-dom';
 import Home from './Page/Home';
-
+import DetalleEmprendedor from './Page/DetalleEmprendedor';
+import formulario from './Page/formulario';
+import AdminEmprendedores from './Page/AdminEmprendedores';
 function App() {
 return (
     <Home />
